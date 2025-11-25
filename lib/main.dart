57ee89da3502510c55021735beefd1585e1c1491
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-25 20:16:00 +0200

    bloc and firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_delivery_app/app.dart';
+import 'package:pizza_delivery_app/simple_bloc_observer.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: App(),
-    );
-  }
-}
 
 
