d0881a4c05d6006de7f00cf083cc13cbdc24ed87
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-25 19:30:01 +0200

    created user entity

---
+++ packages/user_repository/lib/srs/entities/user_entity.dart
@@ -0,0 +1,33 @@
+class MyUserEntity {
+  String userId;
+  String name;
+  String email;
+  bool hasActiveCart;
+
+  MyUserEntity({
+    required this.userId,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+
+  });
+
+  Map<String,Object?> toDocument(){
+    return {
+      'userId':userId,
+      'email':email,
+      'name':name,
+      'hasActiveCart':hasActiveCart
+    };
+  }
+
+  static MyUserEntity fromDocument(Map<String, dynamic> doc){
+    return MyUserEntity(
+        userId:doc ['userId'],
+        email:doc ['email'],
+        name:doc ['name'],
+        hasActiveCart:doc ['hasActiveCart']
+    );
+  }
+
+}
---
+++ packages/user_repository/lib/srs/models/user.dart
@@ -0,0 +1,49 @@
+
+import 'package:user_repository/srs/entities/entities.dart';
+
+class MyUser{
+  String userId;
+  String name;
+  String email;
+  bool hasActiveCart;
+
+  MyUser({
+    required this.userId,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+
+  });
+
+  static final empty = MyUser(
+    userId: '',
+    email: '',
+    name: '',
+    hasActiveCart: false,
+  );
+
+  MyUserEntity toEntity(){
+    return MyUserEntity(
+      userId: userId,
+      email: email,
+      name: name,
+      hasActiveCart: hasActiveCart
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntity entity){
+    return MyUser(
+        userId: entity.userId,
+        name: entity.name,
+        email: entity.email,
+        hasActiveCart: entity.hasActiveCart
+    );
+  }
+
+@override
+  String toString() {
+    return 'MyUser: $userId, $name, $email, $hasActiveCart';
+  }
+
+
+}
